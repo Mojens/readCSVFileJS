e45a9d29f290e083e79ccc736e40b91d14531ecd
Mojens <moha135h@stud.kea.dk>
2022-11-20 03:37:53 +0100

    readFile Done

--- readFile.js
+++ readFile.js
@@ -1,6 +1,15 @@
-function csvToArray(str, delimiter = ";") {
-    const headers = str.slice(0,str.indexOf("\n")-1).split(delimiter);
-    const rows = str.slice(str.indexOf("\n")+1).split("\n");
+const submitForm = document.getElementById("form1")
+const fileUploaded = document.getElementById("uploaded-file")
+
+function sanitizeStringWithTableRows(tableRows) {
+    let secureRows = DOMPurify.sanitize("<table>" + tableRows + "</table>")
+    secureRows = secureRows.replace("<table>", "").replace("</table>", "")
+    return secureRows
+}
+
+function csvFileToArray(str, delimiter = ";") {
+    const headers = str.slice(0, str.indexOf("\n") - 1).split(delimiter);
+    const rows = str.slice(str.indexOf("\n") + 1).split("\n");
     return rows.map(function (row) {
         const values = row.split(delimiter);
         return headers.reduce(function (object, header, index) {
@@ -11,36 +20,26 @@ function csvToArray(str, delimiter = ";") {
 }
 
 function arrayIntoRows(arr) {
-    return arr.map(obj =>
-        `
+    return arr.map(obj => `
 <tr>
     <td>${obj.firstName}</td>
     <td>${obj.lastName}</td>
     <td>${obj.eMail}</td>
-    <td>${obj.phoneNumber}</td>
+    <td>+${obj.phoneNumber}</td>
 </tr>`).join("");
 }
 
+function readFile(evt) {
+    evt.preventDefault();
+    const file = fileUploaded.files[0];
+    const fileReader = new FileReader();
 
-function readFile() {
-    const submitForm = document.getElementById("form1")
-    const fileUploaded = document.getElementById("uploaded-file")
-    if (submitForm != null) {
-        submitForm.addEventListener("submit", function (evt) {
-
-            evt.preventDefault();
-            const file = fileUploaded.files[0];
-            const fileReader = new FileReader();
-
-            fileReader.onload = function (evt) {
-                const text = evt.target.result;
-                const arrayOfObj = csvToArray(text);
-                console.log(arrayOfObj);
-               document.getElementById("file-upload-tbody").innerHTML = arrayIntoRows(arrayOfObj);
-            };
-            fileReader.readAsText(file);
-        });
-    }
+    fileReader.onload = function (evt) {
+        const plainText = evt.target.result;
+        const arrayOfObj = csvFileToArray(plainText);
+        document.getElementById("file-upload-tbody").innerHTML = sanitizeStringWithTableRows(arrayIntoRows(arrayOfObj));
+    };
+    fileReader.readAsText(file);
 }
 
-readFile()
+submitForm.onsubmit = readFile;
